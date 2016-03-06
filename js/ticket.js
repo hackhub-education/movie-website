61453cda2f3d3572c03bf059581874596a1b3fef
Yan Hong <onlyhongyan@gmail.com>
2016-03-06 00:41:17 -0800

    firebase implementation

--- js/ticket.js
+++ js/ticket.js
@@ -8,12 +8,49 @@
 //   }
 // }
 
-$('.ticket-btn').click(function() {
+// $('.ticket-btn').click(function() {
+//   var ticketNum = $($(this).find('span')).html();
+//   // get number of ticket from button span
+//   if (ticketNum > 1) {
+//     $($(this).find('span')).html(ticketNum - 1);
+//     // set number of ticket left
+//   } else {
+//     $(this).addClass('alert-btn').html('Sold Out');
+//     // change text to sold out and add alert button class when ticket number equals zero
+//   }
+// });
+
+var movieData = new Firebase('https://llf9qvcffk4.firebaseio-demo.com/');
+
+movieData.on('child_added', function(snapshot) {
+  movies = snapshot;
+  var movie = snapshot.val();
+  var movieRow = $('<tr>').attr('id', snapshot.key()).appendTo('table');
+  var movieName = $('<td>').addClass('column1').text(movie.name).appendTo(movieRow);
+  var movieDate = $('<td>').text(movie.release_date).appendTo(movieRow);
+  var movieDirector = $('<td>').text(movie.director).appendTo(movieRow);
+  var movieTicket = $('<td>').addClass('center-icon').appendTo(movieRow);
+  
+  if (movie.ticket > 0) {
+    var movieButton = $('<button>').addClass('ticket-btn').appendTo(movieTicket);
+    $('<p><i class="fa fa-ticket fa-2x"></i><span>' + movie.ticket + '</span> Left</p>').appendTo(movieButton);
+  } else {
+    $('<p>').text('Sold Out').appendTo(movieTicket);
+  }
+  
+});
+
+$('body').on('click', '.ticket-btn', function() {
   var ticketNum = $($(this).find('span')).html();
   // get number of ticket from button span
-  if (ticketNum > 1) {
-    $($(this).find('span')).html(ticketNum - 1);
+  if (ticketNum > 0) {
+    var currentTicketNum = ticketNum - 1;
+    $($(this).find('span')).html(currentTicketNum);
     // set number of ticket left
+    var currentMovieId = $(this).parent().parent().attr('id');
+    var currentMovie = new Firebase('https://llf9qvcffk4.firebaseio-demo.com/' + currentMovieId);
+    // Modify the 'first' and 'last' children, but leave other data at fredNameRef unchanged
+    currentMovie.update({ ticket: currentTicketNum });
   } else {
     $(this).addClass('alert-btn').html('Sold Out');
     // change text to sold out and add alert button class when ticket number equals zero
