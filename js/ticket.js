02fe065ff49056a9447f3e312c4561770d4ce7a2
Yan Hong <onlyhongyan@gmail.com>
2016-03-06 17:06:17 -0800

    update ticket

--- js/ticket.js
+++ js/ticket.js
@@ -11,16 +11,32 @@
 var movieDataRef = new Firebase('https://hb5wnko1hrt.firebaseio-demo.com/');
 
 
-$('.ticket-btn').click(function() {
+$('body').on('click', '.ticket-btn', function() {
+
+  var key = $(this).parent().parent().attr('id');
+  console.log(key);
+
+  var currentMovie = new Firebase('https://hb5wnko1hrt.firebaseio-demo.com/' + key);
+
+
   var ticketNum = $($(this).find('span')).html();
   // get number of ticket from button span
-  if (ticketNum > 1) {
-    $($(this).find('span')).html(ticketNum - 1);
+  var currentTicket = ticketNum - 1;
+  if (currentTicket >= 0) {
+
+    if (currentTicket > 0) {
+
+      $($(this).find('span')).html(currentTicket);
+      
+      } else {
+
+      $(this).addClass('alert-btn').html('Sold Out');
+      // change text to sold out and add alert button class when ticket number equals zero
+    }
+
+    currentMovie.update({ticket: currentTicket});
     // set number of ticket left
-  } else {
-    $(this).addClass('alert-btn').html('Sold Out');
-    // change text to sold out and add alert button class when ticket number equals zero
-  }
+  } 
 });
 
 $('#add-movie').click(function() {
@@ -38,21 +54,40 @@ $('#add-movie').click(function() {
 });
 
 movieDataRef.on('child_added', function(snapshot) {
+
    var name = snapshot.val().name;
    var director = snapshot.val().director;
    var date = snapshot.val().release_date;
    var ticket = snapshot.val().ticket;
-   var movieRow = $("<tr>").appendTo('.movie-table');
+   var movieRow = $("<tr>").attr('id', snapshot.key()).appendTo('.movie-table');
    $("<td>").addClass('column1').html(name).appendTo(movieRow);
    $("<td>").html(date).appendTo(movieRow);
    $("<td>").html(director).appendTo(movieRow);
    var buyTicket = $("<td>").addClass('center-icon').appendTo(movieRow);
-   $("<button>").addClass('ticket-btn').html('<i class="fa fa-ticket fa-2x"></i><span>' +  ticket + '</span> Left').appendTo(buyTicket);
+
+   if (ticket > 0) {
+      $("<button>").addClass('ticket-btn').html('<i class="fa fa-ticket fa-2x"></i><span>' +  ticket + '</span> Left').appendTo(buyTicket);
+   } else {
+      $("<p>").html('Sold Out').appendTo(buyTicket);
+   }
+
+   
 });
 
 
 /*
-<tr>
+<tr id="abcdefghijk">
+  <td class="column1">Deadpool</td>
+  <td>February 12, 2016</td>
+  <td>Tim Miller</td>
+  <td class="center-icon">
+  <button class="ticket-btn">
+  <i class="fa fa-ticket fa-2x"></i><span id="deadpool">10</span> Left
+  </button>
+  </td>
+</tr>
+
+<tr id="xyzabcdefg">
   <td class="column1">Deadpool</td>
   <td>February 12, 2016</td>
   <td>Tim Miller</td>
